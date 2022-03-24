9f8af1c99705bc09e1c2c824491035c7c8b10196
Mdu <mbongeni.maikesto@gmail.com>
2022-03-24 22:07:20 +0200

    adding index.js amd package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res)=>{
+    res.send('hello world');
+});
+
+app.listen(3000, ()=>{
+    console.log(` server started and listening on port 3000`);
+});
